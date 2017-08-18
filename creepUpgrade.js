786056d4b46882332eae1fb0dcebdefdef1ac199
Chikara Orval <chikaraorval@pangaea.nl>
2017-08-18 22:50:02 +0200

    fix creepupgrade

--- creepUpgrade.js
+++ creepUpgrade.js
@@ -1,30 +1,30 @@
 module.exports = {
   run(creep) {
-    if (creep.memory.isUpgrading === false && creep.carry.energy === 0) {
-      creep.memory.isUpgrading = true;
+    if (creep.memory.isWorking === false && creep.carry.energy === 0) {
+      creep.memory.isWorking = true;
     } else if (
-      creep.memory.isUpgrading === true &&
+      creep.memory.isWorking === true &&
       creep.carry.energy === creep.carryCapacity
     ) {
-      creep.memory.isUpgrading = false;
+      creep.memory.isWorking = false;
     }
 
-    if (creep.memory.isUpgrading === false) {
+    if (creep.memory.isWorking === false) {
       if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
         creep.moveTo(creep.room.controller);
       }
-    } else {
-      let source;
-      if (
-        Game.spawns.Spawn1.room.energyAvailable ===
-        Game.spawns.Spawn1.room.energyCapacityAvailable
-      ) {
-        source = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-          filter: x => x.structureType === STRUCTURE_SPAWN,
-        });
-      } else {
-        source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+    } else if (
+      Game.spawns.Spawn1.room.energyAvailable ===
+      Game.spawns.Spawn1.room.energyCapacityAvailable
+    ) {
+      const spawn = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+        filter: x => x.structureType === STRUCTURE_SPAWN,
+      });
+      if (creep.withdraw(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(spawn);
       }
+    } else {
+      const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
       if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
         creep.moveTo(source);
       }
