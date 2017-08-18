9641b4efc8423633993880db610cc7ff37c6afd9
Chikara Orval <chikaraorval@pangaea.nl>
2017-08-18 16:23:13 +0200

    Add main loop and create basic harvesting code

---
+++ creepHarvest.js
@@ -0,0 +1,29 @@
+module.exports = {
+  run(creep) {
+    if (creep.memory.isMining === false && creep.memory.energy === 0) {
+      creep.memory.isMining = true;
+    } else if (
+      creep.memory.isMining === true &&
+      creep.carry.energy === creep.carryCapacity
+    ) {
+      creep.memory.isMining = false;
+    }
+
+    if (creep.memory.isMining === true) {
+      const structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+        filter: s => s.energy < s.energyCapacity,
+      });
+
+      if (structure !== undefined) {
+        if (creep.transfer(structure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(structure);
+        }
+      }
+    } else {
+      const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+      if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
+      }
+    }
+  },
+};
---
+++ main.js
@@ -0,0 +1,25 @@
+const creepHarvest = require('./creepHarvest');
+
+module.exports.loop = function() {
+  Object.keys(Game.creeps).forEach(creep => {
+    creep = Game.creeps[creep];
+    if (creep.memory.role === 'harvest') {
+      creepHarvest.run(creep);
+    }
+  });
+
+  const minHarvesters = 4;
+  const currentHarvesters = _.sum(
+    Game.creeps,
+    c => c.memory.role === 'harvester'
+  );
+
+  let name;
+  if (currentHarvesters < minHarvesters) {
+    name = Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE]);
+  }
+
+  if (!(name < 0)) {
+    console.log(`Spawned :${name}`);
+  }
+};
