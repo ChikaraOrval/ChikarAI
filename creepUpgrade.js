48053444a6e7d0e97fde15ac76615920bb416634
Chikara Orval <chikaraorval@pangaea.nl>
2017-08-18 22:29:17 +0200

    get energy from spawn if energy capped

--- creepUpgrade.js
+++ creepUpgrade.js
@@ -1,20 +1,30 @@
 module.exports = {
   run(creep) {
-    if (creep.memory.isWorking === false && creep.carry.energy === 0) {
-      creep.memory.isWorking = true;
+    if (creep.memory.isUpgrading === false && creep.carry.energy === 0) {
+      creep.memory.isUpgrading = true;
     } else if (
-      creep.memory.isWorking === true &&
+      creep.memory.isUpgrading === true &&
       creep.carry.energy === creep.carryCapacity
     ) {
-      creep.memory.isWorking = false;
+      creep.memory.isUpgrading = false;
     }
 
-    if (creep.memory.isWorking === false) {
+    if (creep.memory.isUpgrading === false) {
       if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
         creep.moveTo(creep.room.controller);
       }
     } else {
-      const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+      let source;
+      if (
+        Game.spawns.Spawn1.room.energyAvailable ===
+        Game.spawns.Spawn1.room.energyCapacityAvailable
+      ) {
+        source = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+          filter: x => x.structureType === STRUCTURE_SPAWN,
+        });
+      } else {
+        source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+      }
       if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
         creep.moveTo(source);
       }
