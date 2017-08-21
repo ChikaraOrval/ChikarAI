9f440d877efa879d22a3f0d07f433eaca61c53eb
Chikara Orval <chikaraorval@pangaea.nl>
2017-08-21 16:18:45 +0200

    Add createMiner and createTransporter methods

--- customSpawn.js
+++ customSpawn.js
@@ -2,7 +2,7 @@ module.exports = function() {
   StructureSpawn.prototype.createBalancedCreep = function(energy, roleName) {
     const parts = Math.floor(energy / 200);
     const body = [];
-    for (let i = 0; i < parts; i++) {
+    for (let i = 0; i < parts; i += 1) {
       body.push(WORK);
       body.push(CARRY);
       body.push(MOVE);
@@ -17,13 +17,35 @@ module.exports = function() {
   StructureSpawn.prototype.createHarvester = function(energy) {
     const parts = Math.floor(energy / 150);
     const body = [];
-    for (let i = 0; i < parts; i++) {
+    for (let i = 0; i < parts; i += 1) {
       body.push(WORK);
       body.push(MOVE);
     }
 
     return this.createCreep(body, undefined, {
       role: 'harvest',
+      isWorking: true,
+    });
+  };
+
+  StructureSpawn.prototype.createMiner = function(sourceId) {
+    return this.createCreep([WORK, WORK, WORK, WORK, WORK, MOVE], undefined, {
+      role: 'mine',
+      sourceId,
+    });
+  };
+
+  StructureSpawn.prototype.createTransporter = function(energy) {
+    const parts = Math.floor(energy / 150);
+    const body = [];
+    for (let i = 0; i < parts; i += 1) {
+      body.push(CARRY);
+      body.push(CARRY);
+      body.push(MOVE);
+    }
+
+    return this.createCreep(body, undefined, {
+      role: 'transport',
       isWorking: true,
     });
   };
