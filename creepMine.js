d671631cb481c55b47611f60356722a8cb958250
Chikara Orval <chikaraorval@pangaea.nl>
2017-08-21 16:37:51 +0200

    logs for minjer

--- creepMine.js
+++ creepMine.js
@@ -4,10 +4,14 @@ module.exports = {
     const container = source.pos.findInRange(FIND_STRUCTURES, 1, {
       filter: s => s.structureType === STRUCTURE_CONTAINER,
     });
-
+    console.log('mine 1');
     if (creep.pos.isEqualTo(container.pos)) {
+      console.log('mine 2');
+
       creep.harvest(source);
     } else {
+      console.log('mine 3');
+
       creep.moveTo(container);
     }
   },
