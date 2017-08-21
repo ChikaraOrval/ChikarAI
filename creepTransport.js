bab21352584ba25e45e691e2b5e60ba94837fa9d
Chikara Orval <chikaraorval@pangaea.nl>
2017-08-21 16:19:02 +0200

    Add the transporter creep logic

---
+++ creepTransport.js
@@ -0,0 +1,38 @@
+module.exports = {
+  run(creep) {
+    if (creep.memory.isWorking === false && creep.carry.energy === 0) {
+      creep.memory.isWorking = true;
+    } else if (
+      creep.memory.isWorking === true &&
+      creep.carry.energy === creep.carryCapacity
+    ) {
+      creep.memory.isWorking = false;
+    }
+
+    if (creep.memory.isWorking === false) {
+      const structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+        filter: s =>
+          s.structureType === STRUCTURE_SPAWN ||
+          s.structureType === STRUCTURE_EXTENSION ||
+          (s.structureType === STRUCTURE_TOWER && s.energy < s.energyCapacity),
+      });
+      if (structure !== null) {
+        if (creep.transfer(structure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(structure);
+        }
+      }
+    } else {
+      const container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: s =>
+          s.structureType === STRUCTURE_CONTAINER &&
+          s.store[RESOURCE_ENERGY] > 0,
+      });
+
+      if (container !== null) {
+        if (creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(container);
+        }
+      }
+    }
+  },
+};
