bbf38a991aafc072f5a8674e0a047ca587913e6f
Chikara Orval <chikaraorval@pangaea.nl>
2017-08-23 15:19:09 +0200

    fix attack

--- creepAttack.js
+++ creepAttack.js
@@ -4,9 +4,7 @@ module.exports = {
     const moveFlag = Game.flags.moveFlag;
 
     if (attackFlag) {
-      console.log('attack flag found');
       if (creep.room.name === attackFlag.pos.roomName) {
-        console.log('found');
         const enemy = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if (enemy) {
           if (creep.attack(enemy) === ERR_NOT_IN_RANGE) {
@@ -14,12 +12,8 @@ module.exports = {
           }
         }
       } else {
-        console.log('not found');
         const exit = creep.room.findExitTo(attackFlag.pos.roomName);
-        console.log(exit);
-        const exit2 = creep.room.findExitTo(attackFlag);
-        console.log(exit2);
-        creep.moveTo(exit);
+        creep.moveTo(creep.pos.findClosestByPath(exit));
       }
     } else if (moveFlag) {
       creep.moveTo(moveFlag);
