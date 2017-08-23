96a75e00e8914bfcf893612bd094306954c9b1c6
Chikara Orval <chikaraorval@pangaea.nl>
2017-08-23 15:17:38 +0200

    extra log attack

--- creepAttack.js
+++ creepAttack.js
@@ -2,6 +2,7 @@ module.exports = {
   run(creep) {
     const attackFlag = Game.flags.attackFlag;
     const moveFlag = Game.flags.moveFlag;
+
     if (attackFlag) {
       console.log('attack flag found');
       if (creep.room.name === attackFlag.pos.roomName) {
@@ -15,6 +16,9 @@ module.exports = {
       } else {
         console.log('not found');
         const exit = creep.room.findExitTo(attackFlag.pos.roomName);
+        console.log(exit);
+        const exit2 = creep.room.findExitTo(attackFlag);
+        console.log(exit2);
         creep.moveTo(exit);
       }
     } else if (moveFlag) {
